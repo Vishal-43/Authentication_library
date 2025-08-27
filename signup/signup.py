36d828fa2a7ffdf4fa59ee3f2db33db0cc5b467b
Vishal-43 <vishalsodmise@gmail.com>
2025-08-27 19:34:48 +0530

    working on signup

---
+++ signup/signup.py
@@ -0,0 +1,9 @@
+class signup_manager:
+    def __init__():
+        pass
+
+    def signup(self,email,password):
+        if not email or not password:
+            return [False,'email or password missing']
+        
+        
